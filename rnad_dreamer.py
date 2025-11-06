Fixed wrong percentile calculation and wrong trajectory lenght when sampling from Dreamer

rnad_dreamer.py
@@ -16,7 +16,7 @@ from functools import partial
 
 from dreamer_ma import DreamerMA, DreamerMAOptimizers
 from networks import initialize_rnad_optimizers, RNaDOptimizers, RNaDNetwork, IsetDecoder, Predictor, LegalActionsNetwork, DynamicsPredictor, SequenceModel, JointIsetEncoder
-from train_utils import RNaDConfig, RNaDTimeStep, load_model, save_model, symexp
+from train_utils import RNaDConfig, RNaDTimeStep, load_model, save_model, symexp, get_loss_mean_with_mask
 from distributions import sample_categorical, get_bin_log_prob
 from games.jax_game import GameState
 
@@ -431,7 +431,7 @@ class RNaDDreamer():
                         predictor: Predictor, legal_network: LegalActionsNetwork, encoder: JointIsetEncoder, p1_iset_decoder:IsetDecoder,
                         p2_iset_decoder: IsetDecoder) ->RNaDTimeStep:
     init_chance_sample_key, init_sample_key, trajectory_key, = jax.random.split(key, 3)
-    trajectory_key = jax.random.split(trajectory_key, self.trajectory_max)
+    trajectory_key = jax.random.split(trajectory_key, self.non_chance_trajectory_max)
   
     
     #get initial state from the environment
@@ -701,11 +701,9 @@ class RNaDDreamer():
                                         self.config.lambda_vtrace, self.config.c_vtrace, self.config.rho_vtrace,
                                         self.config.eta, self.config.vtrace_eta, self.config.gamma_vtrace)
       #v_train_target, q_value = jnp.zeros_like(v), jnp.zeros_like(pi)
-      # We multiply by 2, since each player acts
-      normalization = jnp.sum(timestep.valid) * 2
       #v_loss = jnp.sum((expanded_valid * (v - lax.stop_gradient(v_train_target)) ** 2)) / (normalization + (normalization == 0))
       v_loss = -get_bin_log_prob(v_dist_logits, bins, jax.lax.stop_gradient(v_train_target))
-      v_loss_value = jnp.sum(v_loss * expanded_valid) / (normalization + (normalization == 0))
+      v_loss_value = get_loss_mean_with_mask(v_loss, expanded_valid)
 
       # Each Q is multiplied by product of importance_sampling of opponent and inverted sampling policy by the acting player.
       # This computes counterfactual importance sampling
@@ -726,7 +724,7 @@ class RNaDDreamer():
       
       # The multiplication by -1 is critical here, otherwise we would
       # be minimizing the neurd term, but we want to maximize it.
-      neurd_loss_value = -jnp.sum(loss_neurd * expanded_valid) / (normalization + (normalization == 0))
+      neurd_loss_value = -get_loss_mean_with_mask(loss_neurd, expanded_valid)
       return v_loss_value + neurd_loss_value
       
     loss, grad = nnx.value_and_grad(rnad_loss, argnums=0)(optimizers.rnad_optimizer.model, optimizers.rnad_target_optimizer.model, prev_network, _prev_network, timestep, alpha)

rnad_dreamer_joint.py
@@ -14,7 +14,7 @@ from functools import partial
 
 from dreamer_ma import DreamerMA
 from networks import *
-from train_utils import RNaDConfig, RNaDTimeStep, TimeStep, PredictionStepWithLegal, load_model, symexp, get_loss_mean_with_mask
+from train_utils import RNaDConfig, RNaDTimeStep, TimeStep, PredictionStepWithLegal, load_model, symexp, get_loss_mean_with_mask, get_percentiles_with_mask
 from rnad_dreamer import EntropySchedule, neurd_loss, v_trace
 from distributions import sample_categorical, get_bin_log_prob
 
@@ -57,7 +57,6 @@ class RNaDDreamerJoint():
     #Unlike Dreamer, we operate with rewards defined 
     # as (state, action, next_state) and only care how to
     # act in non-terminal states, hence we end one turn before terminal
-    self.trajectory_max = self.world_model.trajectory_max - 1
     self.non_chance_trajectory_max = self.world_model.non_chance_trajectory_max - 1
     self.num_players = self.world_model.game.num_players()
 
@@ -81,8 +80,8 @@ class RNaDDreamerJoint():
     
     self.return_range = jnp.array(0)
 
-    self.optimizers = initialize_joint_optimizers(self.world_model.optimizers, self.config, self.iset_size, self.actions, self.nnx_rngs)
-    self.cached_step = nnx.cached_partial(self._jit_step_with_model, self.optimizers, self.prev_network, self._prev_network)
+    self.optimizers = initialize_rnad_optimizers(self.config, self.iset_size, self.actions, self.nnx_rngs)
+    self.cached_step = nnx.cached_partial(self._jit_step_with_model, self.optimizers, self.world_model.optimizers, self.prev_network, self._prev_network)
     self.learner_steps = 0
     self.policy_switch_steps = 0
   
@@ -188,7 +187,7 @@ class RNaDDreamerJoint():
                         predictor: Predictor, legal_network: LegalActionsNetwork, encoder: JointIsetEncoder, p1_iset_decoder:IsetDecoder,
                         p2_iset_decoder: IsetDecoder) ->RNaDTimeStep:
     #init_sample_key, trajectory_key, = jax.random.split(key)
-    trajectory_key = jax.random.split(key, self.trajectory_max)
+    trajectory_key = jax.random.split(key, self.non_chance_trajectory_max)
   
     
     
@@ -292,7 +291,8 @@ class RNaDDreamerJoint():
   @partial(nnx.jit, static_argnums=(0,))
   def update_parameters_and_model(
     self,
-    optimizers: JointOptimizers,
+    optimizers: RNaDOptimizers,
+    world_model_optimizers: DreamerMAOptimizers,
     prev_network: RNaDNetwork,
     _prev_network: RNaDNetwork,
     trajectory_key,
@@ -337,14 +337,15 @@ class RNaDDreamerJoint():
                                         self.config.lambda_vtrace, self.config.c_vtrace, self.config.rho_vtrace,
                                         self.config.eta, self.config.vtrace_eta, self.config.gamma_vtrace)
       
-      current_range = (jnp.percentile(q_value, 0.95) - jnp.percentile(q_value, 0.05))
+      q_mask = expanded_valid * timestep.legal
+      percentiles = get_percentiles_with_mask(q_value, q_mask, jnp.array([95, 5]))
+      current_range = (percentiles[0] - percentiles[1])
       new_range = 0.01 * current_range + 0.99 * return_range
       #v_train_target, q_value = jnp.zeros_like(v), jnp.zeros_like(pi)
       # We multiply by 2, since each player acts
-      normalization = jnp.sum(timestep.valid) * 2
       #v_loss = jnp.sum((expanded_valid * (v - lax.stop_gradient(v_train_target)) ** 2)) / (normalization + (normalization == 0))
       v_loss = -get_bin_log_prob(v_dist_logits, bins, jax.lax.stop_gradient(v_train_target))
-      v_loss_value = jnp.sum(v_loss * expanded_valid) / (normalization + (normalization == 0))
+      v_loss_value = get_loss_mean_with_mask(v_loss, expanded_valid)
 
       # Each Q is multiplied by product of importance_sampling of opponent and inverted sampling policy by the acting player.
       # This computes counterfactual importance sampling
@@ -365,7 +366,8 @@ class RNaDDreamerJoint():
       
       # The multiplication by -1 is critical here, otherwise we would
       # be minimizing the neurd term, but we want to maximize it.
-      neurd_loss_value = -jnp.sum(loss_neurd * expanded_valid) / (normalization + (normalization == 0))
+      neurd_loss_value = -get_loss_mean_with_mask(loss_neurd, expanded_valid)
+      #jax.debug.breakpoint()
       return v_loss_value + neurd_loss_value, new_range
 
     def imagination_loss(rnad_network: RNaDNetwork,
@@ -447,13 +449,13 @@ class RNaDDreamerJoint():
   
     img_return, grad = nnx.value_and_grad(imagination_loss, argnums=(0), has_aux=True)(
       optimizers.rnad_optimizer.model,
-      optimizers.sequence_optimizer.model,
-      optimizers.dynamics_optimizer.model,
-      optimizers.predictor_optimizer.model,
-      optimizers.legal_actions_optimizer.model,
-      optimizers.encoder_optimizer.model,
-      optimizers.p1_decoder_optimizer.model,
-      optimizers.p2_decoder_optimizer.model, 
+      world_model_optimizers.sequence_optimizer.model,
+      world_model_optimizers.dynamics_optimizer.model,
+      world_model_optimizers.predictor_optimizer.model,
+      world_model_optimizers.legal_actions_optimizer.model,
+      world_model_optimizers.encoder_optimizer.model,
+      world_model_optimizers.p1_decoder_optimizer.model,
+      world_model_optimizers.p2_decoder_optimizer.model, 
       optimizers.rnad_target_optimizer.model,
       prev_network,
       _prev_network,
@@ -512,20 +514,21 @@ class RNaDDreamerJoint():
     return prev_network, _prev_network, img_loss, r_loss, new_range
   
   @partial(nnx.jit, static_argnums=(0))
-  def _jit_step_with_model(self, optimizers: JointOptimizers, prev_network: RNaDNetwork, _prev_network: RNaDNetwork
-                ,trajectory_key, dreamer_timestep: TimeStep, dreamer_prediction_step:PredictionStepWithLegal, return_range:chex.Array, learner_steps: int):
+  def _jit_step_with_model(self, optimizers: JointOptimizers, world_model_optimizers:DreamerMAOptimizers,
+                prev_network: RNaDNetwork, _prev_network: RNaDNetwork,trajectory_key, dreamer_timestep: TimeStep, 
+                dreamer_prediction_step:PredictionStepWithLegal, return_range:chex.Array, learner_steps: int):
     #
     
     alpha, update_regularization = self._entropy_schedule(learner_steps)
     prev_network, _prev_network, img_loss, r_loss, new_range = self.update_parameters_and_model(
-      optimizers, prev_network, _prev_network, trajectory_key, dreamer_timestep, dreamer_prediction_step, return_range, alpha, update_regularization
+      optimizers, world_model_optimizers, prev_network, _prev_network, trajectory_key, dreamer_timestep, dreamer_prediction_step, return_range, alpha, update_regularization
     )
     return prev_network, _prev_network, img_loss, r_loss, new_range, update_regularization
 
   
   def step(self, dreamer_timestep: TimeStep, dreamer_prediction_step:PredictionStepWithLegal):
     trajectory_key = self.get_next_rng_key()
-    #self.prev_network, self._prev_network, img_loss, r_loss, update_regularization =  self._jit_step_with_model(self.optimizers, self.prev_network, self._prev_network, trajectory_key, dreamer_timestep, dreamer_prediction_step, self.return_range, self.learner_steps)
+    #self.prev_network, self._prev_network, img_loss, r_loss, update_regularization =  self._jit_step_with_model(self.optimizers, self.world_model.optimizers, self.prev_network, self._prev_network, trajectory_key, dreamer_timestep, dreamer_prediction_step, self.return_range, self.learner_steps)
     self.prev_network, self._prev_network, img_loss, r_loss, self.return_range, update_regularization = self.cached_step(trajectory_key, dreamer_timestep, dreamer_prediction_step, self.return_range, self.learner_steps)
     self.learner_steps += 1
     self.policy_switch_steps += int(update_regularization)
@@ -557,20 +560,20 @@ class RNaDDreamerJoint():
     self.optimizers = update_nnx(self.optimizers, state["optimizers"])
     self.prev_network = update_nnx(self.prev_network, state["prev_network"])
     self._prev_network = update_nnx(self._prev_network, state["_prev_network"])
-    #Necessary to correctly continue to train on the updated state of the optimizers
-    self.cached_step = nnx.cached_partial(self._jit_step_with_model, self.optimizers, self.prev_network, self._prev_network)
-    #Explicitly reinitialize the world model optimizers
-    # to make sure that the deserialization does not make them point to
-    # two different objects.
-    wm_optimizers = DreamerMAOptimizers(sequence_optimizer= self.optimizers.sequence_optimizer,
-          encoder_optimizer = self.optimizers.encoder_optimizer,
-          p1_decoder_optimizer = self.optimizers.p1_decoder_optimizer,
-          p2_decoder_optimizer = self.optimizers.p2_decoder_optimizer,
-          dynamics_optimizer = self.optimizers.dynamics_optimizer,
-          predictor_optimizer = self.optimizers.predictor_optimizer,
-          legal_actions_optimizer = self.optimizers.legal_actions_optimizer)
+    # #Necessary to correctly continue to train on the updated state of the optimizers
+    # self.cached_step = nnx.cached_partial(self._jit_step_with_model, self.optimizers, self.prev_network, self._prev_network)
+    # #Explicitly reinitialize the world model optimizers
+    # # to make sure that the deserialization does not make them point to
+    # # two different objects.
+    # wm_optimizers = DreamerMAOptimizers(sequence_optimizer= self.optimizers.sequence_optimizer,
+    #       encoder_optimizer = self.optimizers.encoder_optimizer,
+    #       p1_decoder_optimizer = self.optimizers.p1_decoder_optimizer,
+    #       p2_decoder_optimizer = self.optimizers.p2_decoder_optimizer,
+    #       dynamics_optimizer = self.optimizers.dynamics_optimizer,
+    #       predictor_optimizer = self.optimizers.predictor_optimizer,
+    #       legal_actions_optimizer = self.optimizers.legal_actions_optimizer)
 
-    self.world_model.optimizers = wm_optimizers
+    # self.world_model.optimizers = wm_optimizers
     #Do not forget to also reset the cached step of the world model
     self.world_model.cached_step = nnx.cached_partial(self.world_model.world_model_train, self.world_model.optimizers) 
   

train_utils.py
@@ -7,51 +7,6 @@ import pickle
 
 from typing import Sequence, Tuple
 
-
-def symlog(x: chex.Array):
-  return jnp.sign(x) * jnp.log(jnp.abs(x) + 1)
-
-def symexp(x: chex.Array):
-  return jnp.sign(x) * (jnp.exp(jnp.abs(x)) - 1)
-
-def legal_policy(logit: chex.Array, legal: chex.Array):
-  """Get a softmaxed policy out of logit, with
-  zeros at illegal actions. Assumes that these have actions in the last
-  dimension and the same shape."""
-  chex.assert_equal_shape([logit, legal])
-  shifted_logit = logit - logit.max(axis=-1, keepdims=True)
-  exp_logit = jnp.exp(shifted_logit)
-  #The only way this can potentially break is 
-  # if +- inf or NaN appears already in the exp_logit
-  # at which point it is an error in the network
-  masked_exp_logit = exp_logit * legal
-  normalization = jnp.sum(masked_exp_logit, axis=-1, keepdims=True)
-  policy = masked_exp_logit / (normalization + (normalization == 0))
-  return policy
-
-def legal_log_policy(logit: chex.Array, legal: chex.Array):
-  """Uses a legal_policy to get the masked policy
-  and then return a log of it, with the exception
-  of illegal actions which have 0 instead of -inf. 
-  Assumes that these have actions in the last
-  dimension and the same shape."""
-  chex.assert_equal_shape([logit, legal])
-  policy = legal_policy(logit, legal)
-  #The where instead of * legal
-  # is because -inf * 0 would produce NaN
-  log_policy = jnp.where(legal, jnp.log(policy), 0)
-  return log_policy
-
-
-def get_loss_mean_with_mask(loss: chex.Array, mask: chex.Array) -> chex.Array:
-  """Mask a loss using mask and compute its mean, 
-  such that elements with 0 in the mask are correctly ignored.
-    Make sure loss and mask are of broadcastable dimensions. """
-  masked_loss = loss * mask
-  normalization_factor = jnp.sum(mask)
-  summed_loss = jnp.sum(masked_loss)
-  return summed_loss / (normalization_factor + (normalization_factor == 0))
-
   
 
 
@@ -211,6 +166,75 @@ class DreamerMAConfig():
   dynamics_network_details: tuple[int, int] = (256, 1)
   predictor_network_details: tuple[int, int] = (256, 1)
   legal_actions_network_details: tuple[int, int] = (256, 1)
+
+
+
+def symlog(x: chex.Array):
+  return jnp.sign(x) * jnp.log(jnp.abs(x) + 1)
+
+def symexp(x: chex.Array):
+  return jnp.sign(x) * (jnp.exp(jnp.abs(x)) - 1)
+
+def legal_policy(logit: chex.Array, legal: chex.Array):
+  """Get a softmaxed policy out of logit, with
+  zeros at illegal actions. Assumes that these have actions in the last
+  dimension and the same shape."""
+  chex.assert_equal_shape([logit, legal])
+  shifted_logit = logit - logit.max(axis=-1, keepdims=True)
+  exp_logit = jnp.exp(shifted_logit)
+  #The only way this can potentially break is 
+  # if +- inf or NaN appears already in the exp_logit
+  # at which point it is an error in the network
+  masked_exp_logit = exp_logit * legal
+  normalization = jnp.sum(masked_exp_logit, axis=-1, keepdims=True)
+  policy = masked_exp_logit / (normalization + (normalization == 0))
+  return policy
+
+def legal_log_policy(logit: chex.Array, legal: chex.Array):
+  """Uses a legal_policy to get the masked policy
+  and then return a log of it, with the exception
+  of illegal actions which have 0 instead of -inf. 
+  Assumes that these have actions in the last
+  dimension and the same shape."""
+  chex.assert_equal_shape([logit, legal])
+  policy = legal_policy(logit, legal)
+  #The where instead of * legal
+  # is because -inf * 0 would produce NaN
+  log_policy = jnp.where(legal, jnp.log(policy), 0)
+  return log_policy
+
+
+def get_loss_mean_with_mask(loss: chex.Array, mask: chex.Array) -> chex.Array:
+  """Mask a loss using mask and compute its mean, 
+  such that elements with 0 in the mask are correctly ignored.
+    Ensure loss and mask are of broadcastable shape. """
+  broadcasted_mask = jnp.zeros(loss.shape, mask.dtype) + mask
+  masked_loss = loss * broadcasted_mask
+  normalization_factor = jnp.sum(broadcasted_mask)
+  summed_loss = jnp.sum(masked_loss)
+  return summed_loss / (normalization_factor + (normalization_factor == 0))
+
+
+def get_percentiles_with_mask(data: chex.Array, mask:chex.Array, percentile: chex.Array):
+  """Get the given percentile of the data,
+  while ignoring the data given by mask. The percentile
+  should be a number (or array like of numbers) in range (0, 100).
+  The logic is that the percentile should only ensure that the given
+  percent of the unmasked data are under the returned value.
+  Ensure that data and mask are of broadcastable shape.
+     """
+  broadcasted_mask = jnp.zeros(data.shape, mask.dtype) + mask
+  max_val = jnp.max(data)
+  #mask out the invalid values with the maximum value
+  #that way we ensure they will be at the end after sorting
+  masked_data = broadcasted_mask * data + (1 - broadcasted_mask) * max_val
+
+  num_valid = jnp.sum(broadcasted_mask)
+  #Rescale the percentile. To accurately
+  # reflect we do not care about the invalid data at the end
+  scale_factor = num_valid / data.size
+  new_percentile = scale_factor * percentile
+  return jnp.percentile(masked_data, new_percentile)
   
 def get_reference_policy(game_state: GameState, legal_actions: chex.Array):
   """Returns the reference sampling policy. For now returns just a uniform policy.